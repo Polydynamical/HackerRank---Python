e0fe3f617a7671b8f18e390ad15e8f65e496586a
cbracketdash <70626686+cbracketdash@users.noreply.github.com>
2020-09-27 14:12:10 -0700

    Update re_split().py

--- Challenges/re_split().py
+++ Challenges/re_split().py
@@ -3,9 +3,9 @@
 # Your task is to complete the regex_pattern defined below, which will be used to re.split() all of the , and . symbols in s.
 # It’s guaranteed that every comma and every dot in s is preceeded and followed by a digit.
 
-# My unfinished sol:
+# My sol:
 
-regex_pattern = r""
+regex_pattern = r"[.,]"
 
 import re
 print("\n".join(re.split(regex_pattern, input())))
